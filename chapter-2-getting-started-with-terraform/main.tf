69a537f16b0fef7fbdf01fc51dbc5ff7555a5834
Ketouem <ketouem@gmail.com>
2017-05-04 20:23:17 +0200

    Put an ELB in front of the cluster

--- chapter-2-getting-started-with-terraform/main.tf
+++ chapter-2-getting-started-with-terraform/main.tf
@@ -3,9 +3,9 @@ variable "server_port" {
   default = 8080
 }
 
-# output "public_ip" {
-#   value = "${aws_instance.example.public_ip}"
-# }
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
+}
 
 data "aws_availability_zones" "all" {}
 
@@ -44,9 +44,30 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -55,5 +76,26 @@ resource "aws_autoscaling_group" "example" {
     key = "Name"
     value = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:${var.server_port}/"
+    interval = 30
   }
 }
