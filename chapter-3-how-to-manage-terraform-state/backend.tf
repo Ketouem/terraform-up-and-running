39a6b4bbfa1111912ae14e80273f84c48cc2553d
Ketouem <ketouem@gmail.com>
2017-05-09 16:39:55 +0200

    Use the created bucket as a backend

---
+++ chapter-3-how-to-manage-terraform-state/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+    backend "s3" {
+        bucket 		= "terraform-up-and-running-state-065074e5-b2a1-4b8e-9d59-289077a4e250"
+        key 		= "global/s3/terraform.tfstate"
+        region 		= "us-east-1"
+    }
+}
