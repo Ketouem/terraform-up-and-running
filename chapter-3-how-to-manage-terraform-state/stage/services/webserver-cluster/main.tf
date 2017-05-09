0476065e762809c426898932934a3e4aec7abb6e
Ketouem <ketouem@gmail.com>
2017-05-09 18:09:32 +0200

    Reading DB parameter from the webservice with template_file

--- chapter-3-how-to-manage-terraform-state/stage/services/webserver-cluster/main.tf
+++ chapter-3-how-to-manage-terraform-state/stage/services/webserver-cluster/main.tf
@@ -4,16 +4,30 @@ provider "aws" {
 
 data "aws_availability_zones" "all" {}
 
+data "terraform_remote_state" "db" {
+  backend = "s3"
+  config {
+    bucket = "terraform-up-and-running-state-065074e5-b2a1-4b8e-9d59-289077a4e250"
+    key = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+  vars {
+    server_port = "${var.server_port}"
+    db_address = "${data.terraform_remote_state.db.address}"
+    db_port = "${data.terraform_remote_state.db.port}"
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id = "ami-40d28157"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.instance.id}"]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World!" >> index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
-              EOF
+  user_data = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
